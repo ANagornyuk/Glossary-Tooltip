f0cd8eb7f5cf0a0035332e2715c0820ace214c7f
Andrii Nahorniuk <andrii.nahorniuk@smile-ukraine.com>
2026-04-09 23:34:02 +0300

    glossary behavior draft

--- web/modules/custom/glossary_tooltip/js/glossary_tooltip.js
+++ web/modules/custom/glossary_tooltip/js/glossary_tooltip.js
@@ -1,6 +1,34 @@
-(function ($, Drupal, window, document) {
+(function ($, Drupal) {
   "use strict";
 
-  console.log('bla bla');
+  Drupal.behaviors.glossaryTooltip = {
+    attach: function (context, settings) {
+      // Get text content of the node.
+      const node_content_el = $('.node__content > .field--name-body > p', context);
+      let text = node_content_el.text();
 
-})(jQuery, Drupal, window, document);
+      // If no content, nothing to do here.
+      if (!text) {
+        return;
+      }
+
+      let glossary_items = settings.glossaryItems;
+
+      // If no glossary items, nothing to do here.
+      if (glossary_items.length === 0) {
+        return;
+      }
+
+      // Go through all glossaries, find it in text, replace to span for tooltip.
+      for (let item of glossary_items) {
+        item.name;
+
+      }
+
+
+
+
+    }
+  }
+
+})(jQuery, Drupal);
